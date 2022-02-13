94e0198cf246e8d96ad004beb66ca7b5cef63679
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-02-13 13:04:57 -0500

    add services entity

---
+++ casestudy/src/main/java/perscholas/casestudy/database/entity/Services.java
@@ -0,0 +1,20 @@
+package perscholas.casestudy.database.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "services")
+public class Services {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @JoinColumn(name = "description")
+    private String description;
+}
