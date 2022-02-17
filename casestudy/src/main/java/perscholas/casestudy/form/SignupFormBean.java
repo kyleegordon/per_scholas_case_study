64eb83df8ff5832e057119b129740482c510cf3d
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-02-17 09:39:16 -0500

    added error message annotations

--- casestudy/src/main/java/perscholas/casestudy/form/SignupFormBean.java
+++ casestudy/src/main/java/perscholas/casestudy/form/SignupFormBean.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.validator.constraints.Length;
 import perscholas.casestudy.database.entity.User;
+import perscholas.casestudy.validation.EmailUnique;
 import perscholas.casestudy.validation.TwoFieldsAreEqual;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,28 +22,32 @@ public class SignupFormBean {
 
     private Integer id;
 
-    @NotEmpty
+    @NotEmpty(message = "Company Name is required")
     private String companyName;
 
-    @NotEmpty
+    @NotEmpty(message = "Email is required")
+    @Pattern(regexp = "^.+@.+$", message = "Email must match pattern")
+    @EmailUnique(message = "Email must be unique")
     private String email;
 
-    @NotEmpty
+    @NotEmpty(message = "Password is required")
+    @Length(min=5, max=25,
+            message="Password must be between 5 and 25 characters in length")
     private String password;
 
-    @NotEmpty
+    @NotEmpty(message = "Confirm Password is required")
     private String confirmPassword;
 
-    @NotEmpty
+    @NotEmpty(message = "Phone is required")
     private String phone;
 
-    @NotEmpty
+    @NotEmpty(message = "Website is required")
     private String website;
 
-    @NotNull
+    @NotNull(message = "Number of Employees is required")
     private Integer numEmployees;
 
-    @NotEmpty
+    @NotEmpty(message = "Industry is required")
     private String industry;
 
 
